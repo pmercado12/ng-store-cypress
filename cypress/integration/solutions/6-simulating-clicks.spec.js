f564a93cd9a0bee90ec18882e724425c892a4d8d
Alain Chautard <achautard@gmail.com>
2019-08-01 14:43:42 -0700

    Simulating clicks example

--- cypress/integration/solutions/6-simulating-clicks.spec.js
+++ cypress/integration/solutions/6-simulating-clicks.spec.js
@@ -1,12 +1,14 @@
 describe('License plate store home page', () => {
 
-    it('displays the right main title', () => {
+    beforeEach(() => {
         cy.visit('/');
+    });
+
+    it('displays the right main title', () => {
         cy.contains('Welcome to our store').should('be.visible');
     });
 
     it('displays 8 license plates', () => {
-        cy.visit('/');
         cy.get('app-license-plate').should('have.length', 8);
         cy.checkLicensePlateAt(1, '2008 Georgia license plate', '$8');
         cy.checkLicensePlateAt(2, '2015 New Jersey license plate', '$11');
@@ -16,5 +18,18 @@ describe('License plate store home page', () => {
         cy.checkLicensePlateAt(6, '2014 Utah license plate', '$10');
         cy.checkLicensePlateAt(7, '2016 New York license plate', '$9');
         cy.checkLicensePlateAt(8, '2007 Pennsylvania license plate', '$11');
+    });
+
+    it('supports multiple currencies', () => {
+        cy.get('app-currency-switcher').click();
+        cy.contains('EUR').click();
+        cy.checkLicensePlateAt(1, '2008 Georgia license plate', '€9.12');
+        cy.checkLicensePlateAt(2, '2015 New Jersey license plate', '€12.54');
+        cy.checkLicensePlateAt(3, '2013 California My Tahoe license plate', '€10.26');
+        cy.checkLicensePlateAt(4, '2010 Colorado license plate', '€5.7');
+        cy.checkLicensePlateAt(5, '2017 Florida license plate', '€11.4');
+        cy.checkLicensePlateAt(6, '2014 Utah license plate', '€11.4');
+        cy.checkLicensePlateAt(7, '2016 New York license plate', '€10.26');
+        cy.checkLicensePlateAt(8, '2007 Pennsylvania license plate', '€12.54');
     });
 });
