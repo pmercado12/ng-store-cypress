fdcfee76ff1ac2407bd0a9d6de8cbcb32a1e871e
Alain Chautard <achautard@gmail.com>
2019-08-09 15:24:40 -0700

    Updated with exercise #1 solution

--- cypress/integration/solutions/7-mocking-server-calls.spec.js
+++ cypress/integration/solutions/7-mocking-server-calls.spec.js
@@ -12,7 +12,9 @@ describe('License plate store home page', () => {
     it('displays the right main title', () => {
         cy.contains('Welcome to our store')
             .should('be.visible')
-            .should('have.css', 'font-weight', '300');
+            .should('have.css', 'font-weight', '300')
+            .should('have.css', 'font-size', '72px')
+            .and('have.css', 'font-family').and('match', /Segoe UI/);
     });
 
     it('displays 8 license plates', () => {
