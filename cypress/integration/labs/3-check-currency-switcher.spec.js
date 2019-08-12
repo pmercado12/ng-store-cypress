e4822b385ca5c1a3fd5ae68809fc6f7671f31410
Alain Chautard <achautard@gmail.com>
2019-08-12 16:30:46 -0700

    Added lab #3 + solution

--- cypress/integration/labs/3-check-currency-switcher.spec.js
+++ cypress/integration/labs/3-check-currency-switcher.spec.js
@@ -12,6 +12,14 @@ describe('License plate store home page', () => {
             .and('have.css', 'font-family').and('match', /Segoe UI/);
     });
 
+    it('displays 8 license plates', () => {
+        cy.get('app-license-plate').should('have.length', 8);
+        cy.get('app-license-plate:nth-of-type(1)').contains('2008 Georgia license plate').should('be.visible');
+        cy.get('app-license-plate:nth-of-type(1)').contains('$8').should('be.visible');
+        cy.get('app-license-plate:nth-of-type(2)').contains('2015 New Jersey license plate').should('be.visible');
+        cy.get('app-license-plate:nth-of-type(2)').contains('$11').should('be.visible');
+    });
+
     it('can navigate to the cart page', () => {
         cy.contains('My cart').click();
         cy.contains('My Cart').should('be.visible');
@@ -21,17 +29,5 @@ describe('License plate store home page', () => {
     it('can navigate to the checkout page', () => {
         cy.contains('Checkout').click();
         cy.contains('Enter your personal information to complete your order').should('be.visible');
-    });
-
-    it('displays 8 license plates', () => {
-        cy.get('app-license-plate').should('have.length', 8);
-        cy.checkLicensePlateAt(1, '2008 Georgia license plate', '$8');
-        cy.checkLicensePlateAt(2, '2015 New Jersey license plate', '$11');
-        cy.checkLicensePlateAt(3, '2013 California My Tahoe license plate', '$9');
-        cy.checkLicensePlateAt(4, '2010 Colorado license plate', '$5');
-        cy.checkLicensePlateAt(5, '2017 Florida license plate', '$10');
-        cy.checkLicensePlateAt(6, '2014 Utah license plate', '$10');
-        cy.checkLicensePlateAt(7, '2016 New York license plate', '$9');
-        cy.checkLicensePlateAt(8, '2007 Pennsylvania license plate', '$11');
     });
 });
